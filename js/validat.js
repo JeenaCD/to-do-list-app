3610690ae9346a8a6c9286eca93fe75b39d5ef15
JeenaCD <102554603+JeenaCD@users.noreply.github.com>
2022-05-06 19:06:55 +0530

    Update validat.js

---
+++ js/validat.js
@@ -0,0 +1,22 @@
+function callback(p){
+    console.log(p);
+    window.open('./main.html');
+    // window.location.assign('https://jeenacd.github.io/to-do-list-app/main.html');
+    // window.location.assign("./main.html");
+}
+
+function validate(callback){
+    var email=document.getElementById("email");
+    var password=document.getElementById("password");
+    if(email.value !="admin"|| password.value !="12345"){
+        alert("Invalid user");
+        console.log("enter123");
+        // return false;
+    }
+    else{
+        console.log("enter");
+        var s="print";
+    // return true;
+    callback(s);
+    }
+}
